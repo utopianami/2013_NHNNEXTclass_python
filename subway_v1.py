f1f02ca5e6554831ab3d72737e16eac98a862332
youngnam <utopianami@gmail.com>
2013-04-02 00:35:36 +0900

    ver1완성

--- subway_v1.py
+++ subway_v1.py
@@ -21,9 +21,12 @@ ver 2.0
 class human():
 	name = 0
 	age = 0
+	decision = 0
 
 	def brain(self):
-		start_end = input ('환승하시겠습니까? (yes = 1, no = 2)')
+		self.decision = input ('탑승(환승) 하시겠습니까? (yes = 1, no = 2)')
+	def brain2(self):
+		a = input ('탑승(환승) 하시겠습니까? (yes = 1, no = 2)')
 
 class card():
 	basic_price = 0
@@ -40,7 +43,9 @@ class card():
 			self.transfer_price = 100
 
 	def transfer_count(self): #환승카운트 올리기
-		if 0 <= self.transfer_num < 4:
+		if self.transfer_num <0: #처음에 돈이 없어서 문이 열리지 않을때
+			self.transfer_num = 0
+		elif 0 <= self.transfer_num < 4: #환승할때
 			self.transfer_num = self.transfer_num + 1
 		else: # 4초과시 0으로 초기화
 			self.transfer_num = 0
@@ -56,35 +61,29 @@ class station():
 		else: #환승이 아닐경우 환승비용을 0으로 만들기
 			self.payment = Tmoney.basic_price + Tmoney.transfer_price*0
 
-		Tmoney.money = Tmoney.money - self.payment #잔액만들기
-
 	def door(self):
-		if Tmoney.money < Tmoney.basic_price:
-			print '요금이 부족합니다 error ---'
-			Tmoney.money = input('얼마를 충전하시겠습니까? : ')
-
-		else:
+		if  self.payment <= Tmoney.money:
 			print "\n\n-----%s의 카드정보-----" % user.name
 			print "비용 : ", self.payment
+			Tmoney.money = Tmoney.money - self.payment#잔액만들기
 			print "잔액 : " , Tmoney.money
 			print "남은 환승횟수 : ", 4-Tmoney.transfer_num
 			print "-----------------------\n\n"
 
+		else:
+			print '요금이 부족합니다. error ---'
+			print '현재 잔액 : ', Tmoney.money
+			print '필요 금액', self.payment, '\n\n'
+			Tmoney.money = input('얼마를 충전하시겠습니까? : ')
+			Tmoney.transfer_num = Tmoney.transfer_num -1 #환승카운트 올리지 않기
+			
 
-#사람정의 / 카드 구입 / 지하철역 선정
+
+#사람정의 / 카드 구입 / 지하철역 선정 / 환승체크 변수 선언
 user = human()
 Tmoney = card()
 konkuk = station()
-start_end = 1 # 환승 확인
-
-'''
-#test
-user.name = 'A'
-user.age = 2
-Tmoney.money = 50000
-Tmoney.price() # 카드 정보체크
-'''
-
+a = 0
 
 #user 정보 정리 / 카드 충전
 user.name = raw_input("what is your name? : ")
@@ -96,20 +95,13 @@ Tmoney.price() # 카드 정보체크
 #탑승시작
 print ("-----------탑승시작------------\n")
 
-while start_end==1:
-	start_end = 0
-	konkuk.check_payment()
-	konkuk.door()
-	Tmoney.transfer_count()
-	print Tmoney.transfer_num
-	start_end = start_end = input ('환승하시겠습니까? (yes = 1, no = 2)')
+while 1: 
+	konkuk.check_payment() #요금확인 (환승일 경우 & 미환승일 경우)
+	konkuk.door() #기계 프린트()
+	Tmoney.transfer_count() #환승횟수 설정
+	user.brain()
+
+	if user.decision == 2:
+		break
 
 print '\n\n\n이용해주셔서 감사합니다.'
-
-
-'''
-수정사항
-1. 처음 카드를 찍을때 돈이 없을 경우
-	>문이 안열려야 함.
-	>환승카운트가 안되어야 함
-'''
