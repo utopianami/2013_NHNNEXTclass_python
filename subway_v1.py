63023d6a3773a18aeb1277c60c0fb71831f72c3b
youngnam <utopianami@gmail.com>
2013-04-03 00:22:00 +0900

    ver1.1 : 지하철간 환승안됨

--- subway_v1.py
+++ subway_v1.py
@@ -1,14 +1,13 @@
 # -*- coding: UTF-8 -*-
-
 '''
-ver 0.1
+ver 1.0
 >나이/환승여부
 >5번까지 환승가능
 
-ver 1.0
+ver 1.1
 >지하철간 환승안됨
 
-ver 1.1
+ver 1.2
 >같은 버스 환승안됨
 
 ver 2.0
@@ -16,30 +15,28 @@ ver 2.0
 >환승거리별 요금추가 달리
 '''
 
-
-#class 정의 : 사람 / 카드 / 역
+#human_class
 class human():
-	name = 0
-	age = 0
-	decision = 0
+	def __init__(self, name, age, select_traffic):
+		self.name = name
+		self.age = age
+		self.traffic = select_traffic
 
-	def brain(self):
+	def brain(self): #환승여부 생각
 		self.decision = input ('탑승(환승) 하시겠습니까? (yes = 1, no = 2)')
-	
+		if self.decision == 1:
+			self.traffic_transfer = input ('버스? 지하철? (버스 = 1, 지하철 = 2)')
+
+	def complete_transfer(self): #환승 후 이전교통정보를 환승교통정보로 변경 
+		self.traffic = self.traffic_transfer
+
+#card_class
 class card():
-	basic_price = 0
-	transfer_price = 0
-	transfer_num = 0
-	money = 0
-
-	def price(self): #나이에 따라 기본요금/환승요금 설정
-		if user.age == 1:
-			self.basic_price = 900
-			self.transfer_price = 50
-		else:
-			self.basic_price = 1000
-			self.transfer_price = 100
-
+	def __init__(self, age, money): #나이를 연령으로 변경
+		self.money = money
+		self.generation = age/10
+		self.transfer_num = 0
+	
 	def transfer_count(self): #환승카운트 올리기
 		if self.transfer_num <0: #처음에 돈이 없어서 문이 열리지 않을때
 			self.transfer_num = 0
@@ -49,56 +46,78 @@ class card():
 			self.transfer_num = 0
 			print "환승 횟수가 초과되어, 다음번엔 환승이 적용 되지 않습니다."
 
-class station():
+#Station_class
 
+class station():
 	payment = 0
 
-	def check_payment(self):
-		if 0 < Tmoney.transfer_num <= 4: #환승일 경우 기본비용 0만들기
-			self.payment = Tmoney.basic_price*0 + Tmoney.transfer_price
-		else: #환승이 아닐경우 환승비용을 0으로 만들기
-			self.payment = Tmoney.basic_price + Tmoney.transfer_price*0
+	def __init__(self, generation): #연령에 따라 기본요금/환승요금 설정
+		if cardName.generation == 1:
+			self.basic_price = 900
+			self.transfer_price = 50
+		else:
+			self.basic_price = 1000
+			self.transfer_price = 100
 
-	def door(self):
-		if  self.payment <= Tmoney.money:
-			print "\n\n-----%s의 카드정보-----" % user.name
+	def check_traffic(self): # 같은 버스끼리 환승안됨 // 지하철에서 지하철 환승안됨 //
+		if userName.traffic_transfer == 2:
+			if userName.traffic ==2:
+				cardName.transfer_num = 0
+
+	def check_payment(self):
+		if 0 < cardName.transfer_num <= 4: #환승일 경우 기본비용 0만들기
+			self.payment = self.basic_price*0 + self.transfer_price
+		else: #환승이 아닐경우 환승비용을 0으로 만들기
+			self.payment = self.basic_price + self.transfer_price*0
+
+	def activity_door(self):
+		if  self.payment <= cardName.money:
+			print "\n\n-----%s의 카드정보-----" % userName.name
 			print "비용 : ", self.payment
-			Tmoney.money = Tmoney.money - self.payment#잔액만들기
-			print "잔액 : " , Tmoney.money
-			print "남은 환승횟수 : ", 4-Tmoney.transfer_num
+			cardName.money = cardName.money - self.payment#잔액만들기
+			print "잔액 : " , cardName.money
+			print "남은 환승횟수 : ", 4-cardName.transfer_num
 			print "-----------------------\n\n"
 
 		else:
 			print '요금이 부족합니다. error ---'
-			print '현재 잔액 : ', Tmoney.money
+			print '현재 잔액 : ', cardName.money
 			print '필요 금액', self.payment, '\n\n'
-			Tmoney.money = input('얼마를 충전하시겠습니까? : ')
-			Tmoney.transfer_num = Tmoney.transfer_num -1 #환승카운트 올리지 않기
-			
+			cardName.money = input('얼마를 충전하시겠습니까? : ')
+			cardName.transfer_num = cardName.transfer_num -1 #환승카운트 올리지 않기
+	def exit_message(self):
+		print "이용해주셔서 감사합니다."
 
+'''
+activity_door 클래스 내부의 
+cardName.money 를 card.money로 쓸 수 없을까?
+'''
 
-#사람정의 / 카드 구입 / 지하철역 선정 /
-user = human()
-Tmoney = card()
-konkuk = station()
+#bascic_info
+print '-------------------------'
+print '------지하철기본정보-----'
+print '-------------------------'
 
-#user 정보 정리 / 카드 충전
-user.name = raw_input("what is your name? : ")
-user.age = input("당신의 연령은? (청소년=1, 성인 = 2) : ")
-Tmoney.money = input('얼마를 충전하시겠습니까? : ')
-Tmoney.price() # 카드 정보체크
+userName = raw_input("당신의 이름은 무엇입니까?? : ")
+userAge = input("당신의 나이는 몇살입니까? (ex: 17, 22) : ")
+cardName= raw_input("어떤카드를 사용하십니까??\n(ex Tmoney / credit_card / student_card) : ")
+Charege_money = input("얼마를 충전하시겠습니까? : ")
+select_traffic = input ('어떤 교통을 이용하시겠습니까?(버스=1, 지하철=2) : ')
 
+#making object
+userName = human(userName, userAge, select_traffic)
+cardName = card(userName.age, Charege_money)
+stationName = station(cardName.generation)
 
-#탑승시작
-print ("-----------탑승시작------------\n")
+while 1:
 
-while 1: 
-	konkuk.check_payment() #요금확인 (환승일 경우 & 미환승일 경우)
-	konkuk.door() #기계 프린트()
-	Tmoney.transfer_count() #환승횟수 설정
-	user.brain()
-
-	if user.decision == 2:
+	stationName.check_payment() #요금 체크
+	stationName.activity_door() #프린트
+	cardName.transfer_count() #환승카운트
+	userName.brain() #환승여부 물어보기
+	stationName.check_traffic() #환승수단에 따라 환승카운트 조절
+	userName.complete_transfer()
+	if userName.decision ==2:
 		break
 
-print '\n\n\n이용해주셔서 감사합니다.'
+stationName.exit_message()
