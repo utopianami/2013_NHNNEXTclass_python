1a3a69ee9fe409c8a86cd6f08adcb0d5280301f1
youngnam <utopianami@gmail.com>
2013-04-30 15:52:04 +0900

    ing...

--- parsing_error.py
+++ parsing_error.py
@@ -7,21 +7,59 @@ readLine = origin_file.readlines()
 
 #2 count_error
 def count_error(text):
+	index = 0
 	error_num = 0
 	warn_num = 0 
 	notice_num = 0
+	erorr_list = []
 
+	#count_erorr_num
 	for line in text:
 		if line.find('[error]') != -1 :
 			error_num = error_num + 1
+			erorr_list.append(index)
 		elif line.find('[warn]') != -1 :
 			warn_num = warn_num + 1
 		elif line.find('[notice]') != -1 :
 			notice_num = notice_num + 1
+		index = index + 1
 
-	print error_num
-	print warn_num
-	print notice_num
+	print '[SUMMARY REPORT]'
+	print 'error : ', error_num
+	print 'warn : ', warn_num
+	print 'notice : ', notice_num
+
+	#summary report_error
+
+	file_num = 0
+	Invalid_num = 0
+	script_num = 0
+
+
+	for message in text[index]:
+		for word in message:
+			if word == 'file':
+				file_num += 1 
+			elif word == Invalid:
+				Invalid_num += 1
+			elif word == script:
+
+
+
+	print '[SUMMARY REPORT_eroor]'
+	print "File does not exist : "
+	print "Invalid URI in request GET invalid : "
+	print "script not found or unable to stat : "
+	print "client denied by server configuration : "
+	print "attempt to invoke directory as script : " 
+
+
+
+	print '-----erorr_list-----'
+	for i in erorr_list:
+		print readLine[i]
+	print '-------------------'
+
 
 #
 count_error(readLine)
